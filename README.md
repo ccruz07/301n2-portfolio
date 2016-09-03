5e4f1d2fdf83f48f7cd9eec0a80424b08a14daaa
ccruz07 <cruz1706@gmail.com>
2016-09-03 14:37:17 -0700

    Committing changes

--- README.md
+++ README.md
@@ -14,3 +14,9 @@ Aug. 30, 2016 - Worked on js, fixed up some functions to connect submit button &
 Set-up Home and About tabs, content-TBA.
 Changed word doc into html files and connected to event delegation functions.
 Cleaned up CSS file.
+
+Aug. 31, 2016 - worked on js, copied same code from ABE-LSNPLNS, to set up ESOL-LsPlnRes. Still needs more work, can't find file when you open browser and click on drop-down list. All ESOL files are in place, need to get connected to drop-down list.
+Fixed the box display for ESOL, now title and drop-down menu are in same box. Still needs some better alignment - make adjustments w/CSS.
+
+Sept 1 -2, 2016 - worked on class 3 tech requirements, added 2 icons and a metatag. New styles with media queries N/A at this time.
+Fixed drop-down list for ESOL, now connected to html files, set up Next Page button to go to Category B. Js function works to go to next page. Category B Professional Development drop down list is set up with 2 html files ready to connect. Next steps - finish last 2 html files for this category, connect all the files and test it.
--- constructor.js
+++ constructor.js
@@ -2,7 +2,7 @@
 
 var openPage = "container";
 
-function Content(lessonPlans, activities, articles) {                //activities,articles/
+function Content(lessonPlans, activities, articles) {                //Cat A/
   this.lessonPlans = lessonPlans;
   this.activities = activities;
   this.articles = articles;
@@ -14,7 +14,7 @@ function Content(lessonPlans, activities, articles) {                //activitie
 
 var reading = new Content('lessonPlans','ABE-LSNPLNS/ABE Lesson Plans', 'article');
 
-function changeToPage(newPage)
+function changeToPage(newPage)                      //hides Category A from user until clicked//
 {
   $("#"+openPage).fadeOut();
   openPage = newPage;
@@ -25,11 +25,15 @@ $(document).ready(function() {
 
   //$("#selectLabel").html( reading.toHTML() );
 
-  $("#submitPageOne").click(function() {
+  $("#submitPageOne").click(function() {             //click goes from Category A to Category B//
     changeToPage("container2");
   });
 
-  $("#ABE-LSNPLNS").change(function() {
+  $("#nextPage").click(function() {                 //Category B//
+    changeToPage("container3");
+  });
+
+  $("#ABE-LSNPLNS").change(function() {                    //ABE Lesson Plan files//
     var picked = $("#ABE-LSNPLNS").val();
     if(picked == "Lesson Plans")
     {
@@ -45,7 +49,39 @@ $(document).ready(function() {
     }
   });
 
-  $("#mainMenu").click(function(e) {
+  $("#ESOL-LsPlnRes").change(function() {                    //Esol Lesson Plan files//
+    var picked = $("#ESOL-LsPlnRes").val();
+    if(picked == "Lesson Plans")
+    {
+        window.location.href = "./ESOL-LsPlnRes/ESOLindex.html";   //DONE- connected to html files//
+    }
+    else if(picked == "Activities")
+    {
+      window.location.href = "./ESOL Activities/ESOLActivitiesindex.html";  //DONE-connected to html files//
+    }
+    else if(picked == "Articles")
+    {
+      window.location.href = "./ESOL Articles/ESOLArticlesindex.htm";  //DONE-connected to html files//
+    }
+    }
+  });
+
+  //Professional-Development
+
+  var openPage = "container";
+
+  function pD(trainings,conferences,webinars,articles) {      //Cat B-Professional Development//
+    this.trainings = trainings;
+    this.conferences = conferences;
+    this.webinars = webniars;
+    this.articles = articles;
+
+    this.toHTML = function() {
+        return this.trainings + " " + this.conferences + " " + this.webinars + "" + this.articles;
+    }
+  }
+
+  $("#mainMenu").click(function(e) {            
       if(e.target.id == "home")
       {
         changeToPage("container");
