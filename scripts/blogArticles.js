b7c6a62ddcef37822affbce6e0b8e53e8ea1d22b
ccruz07 <cruz1706@gmail.com>
2016-09-06 11:55:41 -0700

    Cruz Portfolio Class04 updates

---
+++ scripts/blogArticles.js
@@ -0,0 +1,27 @@
+﻿//PORTFOLIO blogArticles.js//
+var rawData = [
+  {
+    title: '10 Blogs about Adult Education',
+    category: 'Articles',
+    author: 'Ann Walker',
+    authourUrl: 'https://annwalkwea.wordpress.com/2013/08/10-blogs-about-adult-education',
+    publishedOn: '2013-08-27',
+    body: '<p>There are many excellent blogs about education. Most of those that I’ve come across focus on teaching, learning and leadership in schools and there’s a lot to learn from them, but it’s good to find some that focus specifically on adult education, including part-time adult and community learning.</p>'
+  },
+  {
+  title: 'Adult Education Matters',
+  category: 'Articles',
+  author: 'DG Hendricks',
+  authourUrl: 'adulteducationmatters.blogspot.com',
+  publishedOn: '2016-08-30',
+  body: '<p>Our organization will be hosting its major event April 3-5, 2017 in Washington D.C. where adult learners will get to learn, build relationships and continue to become leaders in the adult literacy field. As a non-profit organization built by adult learners, we hold our Leadership Institute in Capitol Hill every four years to give adult learners the opportunity in meeting directly with decision makers and legislators who can help our cause.</p>'
+},
+{
+  title: 'Reaching Beyond the Adult Education Classroom with Technology',
+  category: 'Articles',
+  author: 'Digital Promises',
+  authorUrl: 'digitalpromise.org/2016/04/20/reaching-beyond-the-adutlt-education-classroom=beyod-the-adult-education',
+  publishedOn: '2016-04-20',
+  body: '<p>Growing up in Oaxaca, Mexico, Octaviano was forced to leave school before completing the first grade. For the next 20 years, his only “academic skill” was the ability to write his own name. A seasonal farm hand, Octaviano continued that work when he moved to the United States, until an accident left him unable to. It was then that he decided to learn English and work toward becoming a U.S. citizen. His plan was solid, except for the fact that he made this decision while living in a rural area that did not offer many educational opportunities for adult learners.</p>'
+},
+]
